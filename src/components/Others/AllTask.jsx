df33b6871025785d76cc5f247b476b7dbacda2f0
Ayush <ayushmandal0122@gmail.com>
2024-11-25 01:03:55 +0530

    adminDashboard completed

---
+++ src/components/Others/AllTask.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const AllTask = () => {
+    return (
+        <div id='tasklist' className='p-5 bg-[#1c1c1c] mt-5 rounded h-48 overflow-auto'>
+            <div className='flex justify-between py-2 px-4 rounded mb-2 bg-red-400'>
+                <h2 className=''>Ayush</h2>
+                <h3>Make UI design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='flex justify-between py-2 px-4 rounded mb-2 bg-green-400'>
+                <h2 className=''>Ayush</h2>
+                <h3>Make UI design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='flex justify-between py-2 px-4 rounded mb-2 bg-blue-400'>
+                <h2 className=''>Ayush</h2>
+                <h3>Make UI design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='flex justify-between py-2 px-4 rounded mb-2 bg-purple-400'>
+                <h2 className=''>Ayush</h2>
+                <h3>Make UI design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='flex justify-between py-2 px-4 rounded mb-2 bg-yellow-400'>
+                <h2 className=''>Ayush</h2>
+                <h3>Make UI design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='flex justify-between py-2 px-4 rounded mb-2 bg-violet-400'>
+                <h2 className=''>Ayush</h2>
+                <h3>Make UI design</h3>
+                <h5>Status</h5>
+            </div>
+        </div>
+    )
+}
+
+export default AllTask
