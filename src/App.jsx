6ac867101925fbfc702c4dba8d37c05343979056
Ayush <ayushmandal0122@gmail.com>
2025-01-07 16:04:04 +0530

    gave specific auth

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,30 @@ import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
 
-  useEffect(() => {
-    // setLocalStorage()
-    getLocalStorage()
-  },)
-
   const [user, setUser] = useState(null)
+  const authData = useContext(AuthContext)
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("loggedInUser")
+    if (loggedInUser) {
+      setUser(loggedInUser.role)
+    }
+  }, [authData]);
+
 
   const handleLogin = (email, password) => {
     if (email == 'admin@me.com' && password == '123') {
       setUser('admin')
-      console.log('admin')
-    } else if (email == 'user@me.com' && password == '123') {
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
+    } else if (authData && authData.employees.find((e) => email == e.email && e.password == password)) {
       setUser('employee')
-      console.log('employee')
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employee' }))
     } else {
       alert("invalid")
     }
   }
 
   // handleLogin(email, password)
-
-  const data = useContext(AuthContext)
-  console.log(data)
 
   return (
     <>
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,11 +1,21 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
+import { getLocalStorage } from '../utils/localStorage'
 
 export const AuthContext = createContext()
 
 const AuthProvider = ({ children }) => {
+
+    const [userData, setUserData] = useState(null)
+    const data = getLocalStorage()
+    useEffect(() => {
+        const { employees, admin } = getLocalStorage()
+        setUserData({ employees, admin })
+    }, [])
+
+
     return (
         <div>
-            <AuthContext.Provider value={'Ayush'}>
+            <AuthContext.Provider value={userData}>
                 {children}
             </AuthContext.Provider>
         </div>
