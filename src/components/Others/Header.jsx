68a5a532f7f83f51a79f83b9dc93d56c40123b1a
Ayush <ayushmandal0122@gmail.com>
2025-01-09 15:57:47 +0530

    setting up who is logged in localStorage v2

--- src/components/Others/Header.jsx
+++ src/components/Others/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({ data }) => {
     console.log(data)
     return (
         <div className='flex items-center justify-between'>
-            <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-emerald-600 text-3xl font-semibold'>Ayush</span></h1>
+            <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-emerald-600 text-3xl font-semibold'>{data.firstName}</span></h1>
             <button className='bg-red-600 text-lg font-medium px-5 py-2 rounded-md'>Logout</button>
         </div>
     )
